b34a1978a84d65e4e8af13d6e887ca07f6f44a09
minguk <minkuk90@gmail.com>
2021-10-02 10:05:53 +0900

    start

---
+++ src/components/Post.js
@@ -0,0 +1,48 @@
+import React from "react";
+// import Grid from "../elements/Grid";
+// import Image from "../elements/image";
+// import Text from "../elements/Text";
+
+import {Grid, Image, Text} from "../elements";
+
+
+const Post = (props) => {
+
+  return (
+    <React.Fragment>
+      <Grid >
+        <Grid is_flex>
+          <Image shape="circle" src={props.src}/>
+          <Text bold>{props.user_info.user_name}</Text>
+          <Text>{props.insert_dt}</Text>
+        </Grid>
+        <Grid padding="16px">
+          <Text>{props.contents}</Text>
+        </Grid>
+        <Grid>
+          <Image shape="rectangle" src={props.src}/>
+        </Grid>
+        <Grid padding="16px">
+          <Text bold>댓글{props.comment_cnt}개</Text>
+        </Grid>
+        <Grid>
+
+        </Grid>
+      </Grid>
+    </React.Fragment>
+  )
+}
+
+Post.defaultProps = {
+  user_info: {
+    user_name: "minguk",
+    user_profile: "https://mblogthumb-phinf.pstatic.net/MjAxOTAzMjlfMzcg/MDAxNTUzODI3OTU3NTg1.geQ7whX1F1DWXBeARbVmBfuqZWp_Db152hko56ZQneIg.5fjMBtfCJeBPcwr1CIndT4qQGbepVcgI9skIDJkEDq8g.JPEG.petianbooks/%EA%B0%95%EC%95%84%EC%A7%80_%EA%B0%B8%EC%9A%B0%EB%9A%B1.jpg?type=w800",
+  },
+  image_url: "https://mblogthumb-phinf.pstatic.net/MjAxOTAzMjlfMzcg/MDAxNTUzODI3OTU3NTg1.geQ7whX1F1DWXBeARbVmBfuqZWp_Db152hko56ZQneIg.5fjMBtfCJeBPcwr1CIndT4qQGbepVcgI9skIDJkEDq8g.JPEG.petianbooks/%EA%B0%95%EC%95%84%EC%A7%80_%EA%B0%B8%EC%9A%B0%EB%9A%B1.jpg?type=w800",
+  contents: "갸우뚱?",
+  comment_cnt: 10,
+  insert_dt: "2021-09-30 17:00:00"
+}
+
+export default Post;
+
---
+++ src/shared/firebase.js
@@ -0,0 +1,19 @@
+import firebase from 'firebase/compat/app'
+import "firebase/compat/auth";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyAY6toOkjjcPja_iiG1kklErobvFv_QMtM",
+  authDomain: "image-community-97660.firebaseapp.com",
+  projectId: "image-community-97660",
+  storageBucket: "image-community-97660.appspot.com",
+  messagingSenderId: "157954951536",
+  appId: "1:157954951536:web:4801ddc0d3eb905e3627b5",
+  measurementId: "G-GXVEENQTPT"
+};
+
+firebase.initializeApp(firebaseConfig);
+
+const apiKey = firebaseConfig.apiKey;
+const auth = firebase.auth();
+
+export{auth, apiKey};
